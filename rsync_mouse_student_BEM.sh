fc8195f12c9e6d3e5e79c9bfa8abded9df1d23fb
tomoakihirakawa <takayuki81610@gmail.com>
2022-10-21 17:16:07 +0900

    Deleted the file from the git repository

--- rsync_mouse_student_BEM.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-name='student@158.215.135.160:/home/student'
-rsync -vr --exclude "CMake*" --exclude "main" --exclude "*.vtu" /Users/tomoaki/Dropbox/markdown/cpp/obj/* ${name}/researchEISPH/cpp/obj/
-rsync -vr --exclude "CMake*" --exclude "main" --exclude "*.vtu" /Users/tomoaki/Dropbox/markdown/cpp/builds/build_sph/* ${name}/researchEISPH/cpp/builds/build_sph/
-rsync -vr --exclude "CMake*" --exclude "*.vtu" /Users/tomoaki/Dropbox/markdown/cpp/include/* ${name}/researchEISPH/cpp/include/
-
-# rsync --update -vr --exclude "*.vtu" /Users/tomoaki/Dropbox/markdown/cpp/builds/*.txt ${name}/research/cpp/builds/
-rsync --update -vr --exclude "CMake*" --exclude "main" /Users/tomoaki/Dropbox/markdown/cpp/builds/build_sph/*.obj ${name}/researchEISPH/cpp/builds/build_sph/
