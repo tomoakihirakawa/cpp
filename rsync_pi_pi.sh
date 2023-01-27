5312b53d5e2ba255d30e2ce6525f4445d4484a02
tomoakihirakawa <takayuki81610@gmail.com>
2023-01-28 00:04:29 +0900

    2023/01/27

--- rsync_pi_pi.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-name='pi@192.168.0.113:/home/pi/'
-rsync -vr --exclude "fundamental*" --exclude "CMakeFiles" --exclude "CMakeCache*" --exclude "main" --exclude "*.vtu" /Users/tomoaki/Dropbox/markdown/python_shared/* ${name}/research/pi/
