32baada151008e5296fd4d496b8617c2b3feb0c1
tomoakihirakawa <takayuki81610@gmail.com>
2023-05-08 22:34:30 +0900

    SPHの修正

--- builds/build_sph/SPH.hpp
+++ builds/build_sph/SPH.hpp
@@ -221,8 +221,8 @@ void test_Bucket(const auto &water, const auto &nets, const std::string &output_
 4. ${{\bf u}^\ast}$と ${{\bf x}^\ast}$を計算
 5. 流速の発散 ${\nabla \cdot {\bf u}^\ast}$の計算
 
-   - Nomeritae et al. (2016)は，${{\bf u}^\ast}$と ${{\bf x}^\ast}$を使っている
-   - Morikawa, D. S., & Asai, M. (2021)，${{\bf u}^\ast}$は使い， ${{\bf x}^\ast}$は使っていない
+   - Nomeritae et al. (2016)は， ${{\bf u}^\ast}$と ${{\bf x}^\ast}$を使っている
+   - Morikawa, D. S., & Asai, M. (2021)， ${{\bf u}^\ast}$は使い， ${{\bf x}^\ast}$は使っていない
 
 6. 流速の発散から密度 ${\rho}^\ast$を計算
 7. 次の時刻の圧力 $p^{n+1}$を計算
