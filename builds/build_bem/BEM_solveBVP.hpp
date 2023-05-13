e8062ecfab9a4435b68aa9d6e00f9ac0c63f694c
tomoakihirakawa <takayuki81610@gmail.com>
2023-05-13 12:19:05 +0900

    READMEの修正

--- builds/build_bem/BEM_solveBVP.hpp
+++ builds/build_bem/BEM_solveBVP.hpp
@@ -230,7 +230,7 @@ void setPhiPhin(Network &water) {
 $\phi$と$\phi_n$に関するBIEは，
 
 $$
-\alpha ({\bf{a}})\phi ({\bf{a}}) = \iint_\Gamma {\left\{ {G({\bf{x}},{\bf{a}})\nabla \phi ({\bf{x}}) - \phi ({\bf{x}})\nabla G({\bf{x}},{\bf{a}})} \right\} \cdot {\bf{n}}({\bf{x}})dS}
+\alpha ({\bf{a}})\phi ({\bf{a}}) = \iint_\Gamma {\left\( {G({\bf{x}},{\bf{a}})\nabla \phi ({\bf{x}}) - \phi ({\bf{x}})\nabla G({\bf{x}},{\bf{a}})} \right\) \cdot {\bf{n}}({\bf{x}})dS}
 \quad\text{on}\quad{\bf x} \in \Gamma(t).
 $$
 
