d3410cfd701b9f50998f967adaab76f479785eec
tomoakihirakawa <takayuki81610@gmail.com>
2023-05-13 21:09:13 +0900

    READMEの修正

--- builds/build_bem/BEM_solveBVP.hpp
+++ builds/build_bem/BEM_solveBVP.hpp
@@ -236,6 +236,19 @@ $$
 
 これを線形三角要素とGauss-Legendre積分で離散化すると，
 
+$`{\alpha_{i_\circ}}{\left( \phi  \right)_{i_\circ}}
+=-\sum\limits_{k_\vartriangle}\sum\limits_{{\xi_1}} {\sum\limits_{{\xi_0}} {\left( {{w_0}{w_1}\left( {\sum\limits_{j=0}^2 {{{\left( {{\phi_n}} \right)}_{k_\vartriangle,j }}{N_{j }}\left( \pmb{\xi } \right)} } \right)\frac{1}{{\| {{\bf{x}}\left( \pmb{\xi } \right) - {{\bf x}_{i_\circ}}} \|}}
+\left\|
+\frac{{\partial{\bf{x}}}}{{\partial{\xi_0}}} \times \frac{{\partial{\bf{x}}}}{{\partial{\xi_1}}}
+\right\|} \right)} }
++ \sum\limits_{k_\vartriangle}\sum\limits_{{\xi_1}} {\sum\limits_{{\xi_0}} {\left( {{w_0}{w_1}\left( {\sum\limits_{j =0}^2{{{\left( \phi  \right)}_{k_\vartriangle,j }}{N_{j }}\left( \pmb{\xi } \right)} } \right)\frac{{{{\bf x}_{i_\circ}} - {\bf{x}}\left( \pmb{\xi } \right)}}{{{{\| {{\bf{x}}\left( \pmb{\xi } \right) - {{\bf x}_{i_\circ}}}\|}^3}}} \cdot
+\left(\frac{{\partial {\bf{x}}}}{{\partial {\xi_0}}}
+\times
+\frac{{\partial {\bf{x}}}}{{\partial {\xi_1}}}
+\right)
+} \right)} }
+`$
+
 $$
 \begin{aligned}
 {\alpha_{i_\circ}}{\left( \phi  \right)_{i_\circ}}
