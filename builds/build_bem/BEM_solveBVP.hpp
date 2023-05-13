f03d83ceca382ecc629675aeee162301ceff8158
tomoakihirakawa <takayuki81610@gmail.com>
2023-05-13 22:33:23 +0900

    READMEの修正

--- builds/build_bem/BEM_solveBVP.hpp
+++ builds/build_bem/BEM_solveBVP.hpp
@@ -245,8 +245,8 @@ $$
 
 $$
 \begin{aligned}
-{\sum\limits_{{\xi_0}} {( {{w_0}{w_1}( {\sum\limits_{j=0}^2 {{{( {{\phi_n}} )}_{k_\vartriangle,j }}{N_{j }}( \pmb{\xi } )} } )\frac{1}{{\| {{\bf{x}}( \pmb{\xi } ) - {{\bf x}_{i_\circ}}} \|}}
-\|\frac{{\partial{\bf{x}}}}{{\partial{\xi_0}}} \times \frac{{\partial{\bf{x}}}}{{\partial{\xi_1}}}
+{\sum\limits\_{{\xi\_0}} {( {{w\_0}{w\_1}( {\sum\limits\_{j=0}^2 {{{( {{\phi\_n}} )}\_{k\_\vartriangle,j }}{N\_{j }}( \pmb{\xi } )} } )\frac{1}{{\| {{\bf{x}}( \pmb{\xi } ) - {{\bf x}\_{i_\circ}}} \|}}
+\|\frac{{\partial{\bf{x}}}}{{\partial{\xi\_0}}} \times \frac{{\partial{\bf{x}}}}{{\partial{\xi_1}}}
 \|} )}}
 \end{aligned}
 $$
