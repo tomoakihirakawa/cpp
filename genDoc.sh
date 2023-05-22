ca092440a46a7023762554b725660230b53b98d4
tomoakihirakawa <takayuki81610@gmail.com>
2023-05-23 08:28:02 +0900

    update SPH

--- genDoc.sh
+++ genDoc.sh
@@ -3,11 +3,11 @@
 python3 ./extract_comments.py README.md ./
 echo 'python3 ./extract_comments.py README.md ./'
 
-(cd builds/build_sph; python3 ../../extract_comments.py README.md ./ ../)
-echo '(cd builds/build_sph; python3 ../../extract_comments.py README.md ./ ../)'
+(cd builds/build_sph; python3 ../../extract_comments.py README.md ./ ../../)
+echo '(cd builds/build_sph; python3 ../../extract_comments.py README.md ./ ../../)'
 
-(cd builds/build_bem; python3 ../../extract_comments.py README.md ./ ../)
-echo '(cd builds/build_bem; python3 ../../extract_comments.py README.md ./ ../)'
+(cd builds/build_bem; python3 ../../extract_comments.py README.md ./ ../../)
+echo '(cd builds/build_bem; python3 ../../extract_comments.py README.md ./ ../../)'
 
-(cd builds/build_ODE; python3 ../../extract_comments.py README.md ./ ../)
-echo '(cd builds/build_ODE; python3 ../../extract_comments.py README.md ./ ../)'
+(cd builds/build_ODE; python3 ../../extract_comments.py README.md ./ ../../)
+echo '(cd builds/build_ODE; python3 ../../extract_comments.py README.md ./ ../../)'
