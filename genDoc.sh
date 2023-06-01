f8f4eb965874230bf4fb67e1a887e0edc183e456
tomoakihirakawa <takayuki81610@gmail.com>
2023-06-01 23:46:11 +0900

    update README.md

--- genDoc.sh
+++ genDoc.sh
@@ -14,3 +14,5 @@ echo '(cd builds/build_ODE; python3 ../../extract_comments.py README.md ./ ../..
 
 (cd builds/build_divide_merge; python3 ../../extract_comments.py README.md ./ ../../)
 echo '(cd builds/build_divide_merge; python3 ../../extract_comments.py README.md ./ ../../)'
+
+python3 generate_readme.py
