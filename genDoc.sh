2e7ef235ba8ad55e5f50f9de0707a13c6bbc935e
tomoakihirakawa <takayuki81610@gmail.com>
2023-06-02 13:08:53 +0900

    update readme

--- genDoc.sh
+++ genDoc.sh
@@ -21,5 +21,7 @@ echo '(cd builds/build_spherical_harmonic; python3 ../../extract_comments.py REA
 (cd builds/build_system_of_linear_eqs; python3 ../../extract_comments.py README.md ./ ../../)
 echo '(cd builds/build_system_of_linear_eqs; python3 ../../extract_comments.py README.md ./ ../../)'
 
+(cd builds/build_system_of_linear_eqs; python3 ../../extract_comments.py README.md ./ ../../)
+echo '(cd builds/build_system_of_linear_eqs; python3 ../../extract_comments.py README.md ./ ../../)'
 
 python3 generate_readme.py
