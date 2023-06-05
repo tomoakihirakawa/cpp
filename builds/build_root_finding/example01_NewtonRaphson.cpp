d74bb38a677d00a08eb8dde239ffb442316f9dc5
tomoakihirakawa <takayuki81610@gmail.com>
2023-06-05 19:32:04 +0900

    update doc LightHill

--- builds/build_root_finding/example01_NewtonRaphson.cpp
+++ builds/build_root_finding/example01_NewtonRaphson.cpp
@@ -11,19 +11,19 @@ struct LightHillRobot {
    double k;
    double c1;
    double c2;
-   int n;
+   int n;  // node + 1 (head node is dummy)
 
    LightHillRobot(double L, double w, double k, double c1, double c2, int n)
-       : L(L), w(w), k(k), c1(c1), c2(c2), n(n){};
+       : L(L), w(w), k(k), c1(c1), c2(c2), n(n + 1){};
 
    auto yLH(const double x, const double t) { return (c1 * x / L + c2 * std::pow(x / L, 2)) * sin(k * (x / L) - w * t); };
 
-   auto X_RB(const std::array<double, 2> a, const double q) {
+   auto X_RB(const std::array<double, 2>& a, const double q) {
       double r = L / n;
       return a + r * std::array<double, 2>{cos(q), sin(q)};
    };
 
-   auto f(const std::array<double, 2> a, const double q, const double t) {
+   auto f(const std::array<double, 2>& a, const double q, const double t) {
       auto [x, y] = X_RB(a, q);
       return yLH(x, t) - y;
    };
@@ -42,14 +42,14 @@ struct LightHillRobot {
    V_d getAngles(const double t) {
       std::vector<double> Q(n, 0.);  // thetas
       std::array<double, 2> a{{0., 0.}};
-      double error = 0;
-      for (auto i = 1; i < Q.size(); i++) {
+      double error = 0, F;
+      for (auto i = 0; i < Q.size(); i++) {
          NewtonRaphson nr(Q[i - 1]);
          error = 0;
          for (auto k = 0; k < 50; k++) {
-            auto F = f(a, nr.X, t);
+            F = f(a, nr.X, t);
             nr.update(F * F / 2., F * ddq_f(nr.X, t));
-            if ((error = std::abs(F)) < 1E-5)
+            if ((error = std::abs(F)) < 1E-4)
                break;
          }
          Q[i] = nr.X;
@@ -58,12 +58,13 @@ struct LightHillRobot {
       return Q;
    };
 
-   std::vector<std::array<double, 2>> anglesToX(const V_d Q) {
+   std::vector<std::array<double, 2>> anglesToX(const V_d& Q) {
       std::array<double, 2> a = {0., 0.};
-      std::vector<std::array<double, 2>> ret(Q.size());
-      ret[0] = a;
-      for (auto i = 1; i < Q.size(); i++)
-         ret[i] = (a = X_RB(a, Q[i]));
+      std::vector<std::array<double, 2>> ret;
+      ret.reserve(Q.size() + 1);
+      ret.push_back(a);
+      for (auto i = 0; i < Q.size(); i++)
+         ret.push_back(a = X_RB(a, Q[i]));
       return ret;
    };
 };
@@ -115,35 +116,44 @@ int main() {
 
     NOTE: この目的関数$f$には，前の節の位置を与える必要がある．節の位置は，後ろの節の位置によって変わらないので，この目的関数を先頭から順番に最適化することは問題ない．
 
-   ![sample.gif](sample.gif)
+   | $n=5$ | $n=10$ | $n=50$ |
+   |:---:|:---:|:---:|
+   | ![sample5.gif](sample5.gif)  | ![sample10.gif](sample10.gif) | ![sample50.gif](sample50.gif) |
 
    NOTE: ただし，$f$を目的関数とすると根への収束が良くなかったので，$f^2/2$を目的関数として計算した．目的関数の微分は，$f \frac{df}{d\theta}$としている．
 
     */
-
-   TimeWatch time;
-
-   std::cout << time() << std::endl;
 
    double L = 0.71;
    double w = 2. * M_PI * 1.0;
    double k = 2. * M_PI * 2.0;
    double c1 = 0.05;
    double c2 = 0.05;
-   int nodes = 10;
+   int nodes = 5;
    int steps = 20;
-
    LightHillRobot lhr(L, w, k, c1, c2, nodes);
-
    for (auto i = 0; i < steps; i++) {
       std::ofstream outFile("./output_lighthill/lighthill" + std::to_string(i) + ".txt");
+      TimeWatch time;
       double t = (double)i / steps;
       auto Q = lhr.getAngles(t);
       auto xy = lhr.anglesToX(Q);
+      std::cout << "time : " << time() << std::endl;
       for (auto j = 0; j < xy.size(); j++) {
          auto [x, y] = xy[j];
-         outFile << x << " " << y << " " << Q[j] << std::endl;
-         std::cout << x << " " << y << " " << Q[j] << std::endl;
+         outFile << j << " " << x << " " << y << " " << Q[j] << std::endl;
+         std::cout << j << " " << x << " " << y << " " << Q[j] << std::endl;
+      }
+      outFile.close();
+   }
+
+   // true value
+   for (auto i = 0; i < steps; i++) {
+      std::ofstream outFile("./output_lighthill/lighthill" + std::to_string(i) + "_analitical.txt");
+      double t = (double)i / steps;
+      for (auto j = 0; j < 1000; j++) {
+         double x = (double)j / 1000;
+         outFile << x << " " << lhr.yLH(x, t) << std::endl;
       }
       outFile.close();
    }
--- builds/build_root_finding/plot_lighthill.py
+++ builds/build_root_finding/plot_lighthill.py
@@ -1,23 +1,29 @@
 import os
 import matplotlib.pyplot as plt
-import imageio.v2 as imageio  # This is the corrected line
-
-# Function to read data from a file and plot it
+import imageio.v2 as imageio
+import numpy as np
 
 plt.rcParams["font.family"] = "Times New Roman"
 
-# Function to read data from a file and plot it
 
-
-def plot_data(file_name):
-    x_data, y_data = [], []
+def plot_data(file_name, analitical_file_name):
+    i_data, x_data, y_data = [], [], []
 
     # Read data from file
     with open(file_name, 'r') as f:
         for line in f:
-            x, y, q = map(float, line.strip().split())
+            index, x, y, q = map(float, line.strip().split())
+            i_data.append(index)
             x_data.append(x)
             y_data.append(y)
+
+    # Read analytical data
+    x_analitical, y_analitical = [], []
+    with open(analitical_file_name, 'r') as f:
+        for line in f:
+            x, y = map(float, line.strip().split())
+            x_analitical.append(x)
+            y_analitical.append(y)
 
     # Create plot
     fig, ax = plt.subplots(figsize=(8, 3))
@@ -27,7 +33,11 @@ def plot_data(file_name):
     ax.set_ylim(-0.1, 0.1)
 
     # Plot point with larger size
-    ax.scatter(x_data, y_data, s=50, label='node')
+    ax.scatter(x_data[1:-1], y_data[1:-1], s=50, label='node')
+
+    for i, txt in enumerate(y_data[1:-1]):
+        ax.text(x_data[i+1], y_data[i+1],
+                str(int(i_data[i+1])), fontsize=12, color='red')
 
     # Aspect ratio
     ax.set_aspect('equal', adjustable='box')
@@ -36,7 +46,10 @@ def plot_data(file_name):
     ax.grid()
 
     # Plot line
-    ax.plot(x_data, y_data)
+    ax.plot(x_data, y_data, label='Numerical Solution')
+
+    # Plot analytical solution
+    ax.plot(x_analitical, y_analitical, label='Analytical Solution')
 
     # Labels and title with larger font size and times
     ax.set_xlabel('x', fontsize=12, fontname='Times New Roman')
@@ -44,7 +57,7 @@ def plot_data(file_name):
     ax.set_title(file_name, fontsize=14, fontname='Times New Roman')
 
     # Legend
-    ax.legend()
+    ax.legend(loc='upper right')
 
     # Save the figure
     plt.savefig(file_name + ".png")
@@ -57,7 +70,8 @@ def plot_data(file_name):
 dir_name = "./output_lighthill/"
 
 # List of data files
-files = [f for f in os.listdir(dir_name) if f.endswith('.txt')]
+files = [f for f in os.listdir(dir_name) if f.endswith(
+    '.txt') and not f.endswith('_analitical.txt')]
 
 # Sort files to ensure they're processed in order
 files.sort(key=lambda x: int(x.replace('lighthill', '').replace('.txt', '')))
@@ -66,9 +80,8 @@ files.sort(key=lambda x: int(x.replace('lighthill', '').replace('.txt', '')))
 images = []
 
 for file_name in files:
-    plot_data(dir_name + file_name)
-    # This is the corrected line
+    analitical_file_name = file_name.replace('.txt', '_analitical.txt')
+    plot_data(dir_name + file_name, dir_name + analitical_file_name)
     images.append(imageio.imread(dir_name + file_name + ".png"))
 
 imageio.mimsave('sample.gif', images, loop=100)
-# Save as a GIF
