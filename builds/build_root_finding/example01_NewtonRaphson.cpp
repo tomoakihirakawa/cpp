df9d8d250919e13910692da334be58c122d57aa5
tomoakihirakawa <takayuki81610@gmail.com>
2023-06-05 17:58:22 +0900

    update BEM doc

--- builds/build_root_finding/example01_NewtonRaphson.cpp
+++ builds/build_root_finding/example01_NewtonRaphson.cpp
@@ -104,7 +104,7 @@ int main() {
 
     NOTE: この目的関数$f$には，前の節の位置を与える必要がある．節の位置は，後ろの節の位置によって変わらないので，この目的関数を先頭から順番に最適化することは問題ない．
 
-   ![./output_lighthill/robot_movement.gif](./output_lighthill/robot_movement.gif)
+   ![./output_lighthill/sample.gif](./output_lighthill/sample.gif)
 
     */
 
