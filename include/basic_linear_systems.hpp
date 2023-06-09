7a5c4418ea5e879d8da167910852fe01e219a0b8
tomoakihirakawa <takayuki81610@gmail.com>
2023-06-10 00:31:37 +0900

    update Arnoldiprocess doc

--- include/basic_linear_systems.hpp
+++ include/basic_linear_systems.hpp
@@ -761,39 +761,32 @@ struct ILU {
 /* ------------------------------------------------------ */
 /*DOC_EXTRACT ArnoldiProcess
 
-## Arnoldi Process
+## Arnoldi過程
 
-ヘッセンベルグ行列$H[0:k-1]$は，Aと相似なベクトルであり，同じ固有値を持つ
-GMRESで使う場合，$V0$にはNormalize(b-A.x0)を与える．
-x0は初期値
-
-アーノルディ法は固有値問題の数値解法であり反復解法．
-一般的な行列の固有ベクトルと固有値をクリロフ空間の直行基底によって近似する方法計算する方法．
-
-1. 正規化した${\bf v}_1$を与えておく．
-2. ${\bf v}_2 = {\rm Normalize}(\,\,\,\quad\quad\quad\quad\quad A{\bf v}_1 - ((A{\bf v}_1) \cdot {\bf v}_1){\bf v}_1\,\,\qquad\qquad\qquad\qquad\qquad\qquad)$を計算する．
-3. ${\bf v}_3 = {\rm Normalize}(\quad\quad\quad({\bf w}=A{\bf v}_2 - ((A{\bf v}_2) \cdot {\bf v}_1){\bf v}_1)) - ({\bf w} \cdot {\bf v}_2){\bf v}_2\quad\quad\quad\quad\quad\quad)$を計算する．
-4. ${\bf v}_4 = {\rm Normalize}(({\bf w}=(({\bf w}=A{\bf v}_3 - ((A{\bf v}_3) \cdot {\bf v}_1){\bf v}_1)) - ({\bf w} \cdot {\bf v}_2){\bf v}_2)) - ({\bf w} \cdot {\bf v}_3){\bf v}_3)$を計算する．
+1. 正規化した$`{\bf v}_1`$を与えておく．
+2. $`{\bf v}_2 = {\rm Normalize}(\,\,\,\quad\quad\quad\quad\quad A{\bf v}_1 - ((A{\bf v}_1) \cdot {\bf v}_1){\bf v}_1\,\,\qquad\qquad\qquad\qquad\qquad\qquad)`$を計算する．
+3. $`{\bf v}_3 = {\rm Normalize}(\quad\quad\quad({\bf w}=A{\bf v}_2 - ((A{\bf v}_2) \cdot {\bf v}_1){\bf v}_1)) - ({\bf w} \cdot {\bf v}_2){\bf v}_2\quad\quad\quad\quad\quad\quad)`$を計算する．
+4. $`{\bf v}_4 = {\rm Normalize}(({\bf w}=(({\bf w}=A{\bf v}_3 - ((A{\bf v}_3) \cdot {\bf v}_1){\bf v}_1)) - ({\bf w} \cdot {\bf v}_2){\bf v}_2)) - ({\bf w} \cdot {\bf v}_3){\bf v}_3)`$を計算する．
 
 言い換えると，
 
-1. 正規化した${\bf v}_1$を与えておく．
-2. ${\bf w}=A{\bf v}_1, {\bf v}_2 = {\rm Normalize}({\rm Chop}({\bf w},{\bf v}_1))$を計算する．
-3. ${\bf w}=A{\bf v}_2, {\bf v}_3 = {\rm Normalize}({\rm Chop}({\rm Chop}({\bf w}, {\bf v}_1), {\bf v}_2))$を計算する．
-4. ${\bf w}=A{\bf v}_3, {\bf v}_4 = {\rm Normalize}({\rm Chop}({\rm Chop}({\rm Chop}({\bf w}, {\bf v}_1), {\bf v}_2), {\bf v}_3))$を計算する．
+1. 正規化した$`{\bf v}_1`$を与えておく．
+2. $`{\bf w}=A{\bf v}_1, {\bf v}_2 = {\rm Normalize}({\rm Chop}({\bf w},{\bf v}_1))`$を計算する．
+3. $`{\bf w}=A{\bf v}_2, {\bf v}_3 = {\rm Normalize}({\rm Chop}({\rm Chop}({\bf w}, {\bf v}_1), {\bf v}_2))`$を計算する．
+4. $`{\bf w}=A{\bf v}_3, {\bf v}_4 = {\rm Normalize}({\rm Chop}({\rm Chop}({\rm Chop}({\bf w}, {\bf v}_1), {\bf v}_2), {\bf v}_3))`$を計算する．
 
-$A{\bf v}_i$の直交化の際に，
-それに含まれる各基底${\bf v}_0,{\bf v}_1,...,{\bf v}_i$の成分を計算している．
+$`A{\bf v}_i`$の直交化の際に，
+それに含まれる各基底$`{\bf v}_0,{\bf v}_1,...,{\bf v}_i`$の成分を計算している．
 この成分からなる行列が，Hessenberg行列$H$である．
 
-$$
+```math
 \begin{align*}
 A{\bf v}_1 & = h_{1,1} {\bf v}_1 + h_{2,1} {\bf v}_2\\
 A{\bf v}_2 & = h_{1,2} {\bf v}_1 + h_{2,2} {\bf v}_2 + h_{3,2} {\bf v}_3\\
 & \dots\\
 A{\bf v}_{n} & = h_{1,n} {\bf v}_1 + h_{2,n} {\bf v}_2 + \cdots + h_{n,n+1} {\bf v}_{n+1}
 \end{align*}
-$$
+```
 
 行列を使ってまとめると，
 
@@ -802,8 +795,7 @@ A V_n = V_{n+1} \tilde H_n, \quad V_n = [v_1|v_2|...|v_n],
 \quad \tilde H_n = \begin{bmatrix} h_{1,1} & h_{1,2} & \cdots & h_{1,n} & h_{1,n+1} \\ h_{2,1} & h_{2,2} & \cdots & h_{2,n} & h_{2,n+1} \\ \vdots & \vdots & \ddots & \vdots & \vdots \\ 0 & 0 & \cdots & h_{n,n} & h_{n,n+1} \\ 0 & 0 & \cdots & 0 & h_{n+1,n+1} \end{bmatrix}
 ```
 
-これをArnoldi分解という．ここで，$[v_1|v_2|...|v_n]$
-の$|$は列ベクトルを連結して行列を形成することを示している．
+これをArnoldi分解という．ここで，$`[v_1|v_2|...|v_n]`$の$`|`$は列ベクトルを連結して行列を形成することを示している．
 
 */
 template <typename Matrix>
@@ -840,39 +832,22 @@ struct ArnoldiProcess {
 
 ## 一般化最小残差法/GMRES
 
-- ヘッセンベルグ行列$H$
-- クリロフ部分空間の直交基底$V$
-- $H$をQR分解した行列$Q$と$R$
-- $g$は行列$Q$の最初の列
-
-ArnoldiProcessによって，$H$と$V$を求める．このArnoldiProcessクラスの派生クラスとしてGMRESを定義している．
-
-残差${\bf r} = {\bf b} - A{\bf x}_n$を最小とするような${\bf x}_n$を求める．
+残差$`\|{\bf b} - A{\bf x}_n\|`$を最小とするような$`{\bf x}_n`$を求める．
 
 ```math
-{\bf r} = {\bf b} - A{\bf x}_n
+\begin{align*}
+\|{\bf b} - A{\bf x}_n\| & = \|{\bf b} - A V_n {\bf y}_n\|\\
+& = \|{\bf b} - V_{n+1} \tilde H_n {\bf y}_n\|\quad \text{(use Arnoldi decomposition)}\\
+& = \|V_{n+1} (\|{\bf b}\| {\bf e}_1 - \tilde H_n {\bf y}_n)\|\\
+& = \|\|{\bf b}\| {\bf e}_1 - \tilde H_n {\bf y}_n\|\\
+& = \|\|{\bf b}\| {\bf e}_1 - QR {\bf y}_n\|\quad \text{(use QR decomposition)}\\
+\end{align*}
 ```
 
-```math
-{\bf r} = {\bf b} - A V_n {\bf y}_n
-```
-
-
-```math
-A V_n {\bf y}_n = V_{n+1} \tilde H_n {\bf y}_n
-```
-
-```math
-{\bf b} - A V_n {\bf y}_n = {\bf b} - V_{n+1} \tilde H_n {\bf y}_n
-```
-
-答えとして，$\|{\bf b} - V_{n+1} \tilde H_n {\bf y}_n\|$を最小とするような，
-${\bf y}_n$を求める．
-
-```math
-{\bf x}_n \approx V_n {\bf y}_n
-```
-
+1. クリロフ部分空間法の考えから，$`\|{\bf b} - A V_n {\bf y}_n\|`$を最小とするような，$`{\bf y}_n`$を求める問題に書き換える．
+2. Arnoldi分解を使って，$`A V_n = V_{n+1} \tilde H_n`$と書き換える．
+3. $`V_{n+1}`$でくくる．
+4. QR分解を使って，$`{\bf y}_n`$に関する最小二乗問題を$`{\bf y}_n`$について解く．
 
 */
 
