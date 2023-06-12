81fccd77bea96eb9effa8f2f5ef3f1ea3da4cf3c
tomoakihirakawa <takayuki81610@gmail.com>
2023-06-12 14:43:24 +0900

    update spherical harmonic doc

--- builds/build_spherical_harmonic/test_multipole_expansion.cpp
+++ builds/build_spherical_harmonic/test_multipole_expansion.cpp
@@ -49,7 +49,7 @@ std::array<double, 3> gradG(const std::array<double, 3>& X_near, const std::arra
 
 ### 球面座標系への変換
 
-${\bf x}=(x,y,z)$から球面座標$(r,a,b)$への変換は次のように行う．
+$`{\bf x}=(x,y,z)`$から球面座標$`(r,a,b)`$への変換は次のように行う．
 
 $$
 r = \|{\bf x}\|, \quad a = \arctan \frac{\sqrt{x^2 + y^2}}{z}, \quad b = \arctan \frac{y}{x}
@@ -86,18 +86,18 @@ std::array<std::array<double, 3>, 3> gradSphericalCoordinates(const std::array<d
 
 ### $G_{\rm apx}$の精度
 
-${\bf c}=(x,y,0)$を変化させてプロットした結果：
+$`{\bf c}=(x,y,0)`$を変化させてプロットした結果：
 
 | | **n=4** | **n=5** | **n=6** | **n=7** | **n=8** |
 |:----:|:---:|:---:|:---:|:---:|:---:|
 | **$`{\bf x} = (0,0,0),{\bf a} = (5,5,5)`$** | ![n4_A_5_5_5](./output_n4_A_5_5_5.png) | ![n5_A_5_5_5](./output_n5_A_5_5_5.png) | ![n6_A_5_5_5](./output_n6_A_5_5_5.png) | ![n7_A_5_5_5](./output_n7_A_5_5_5.png) | ![n8_A_5_5_5](./output_n8_A_5_5_5.png) |
 | **$`{\bf x} = (0,0,0),{\bf a} = (10,10,10)`$** | ![n4_A_10_10_10](./output_n4_A_10_10_10.png) | ![n5_A_10_10_10](./output_n5_A_10_10_10.png)  | ![n6_A_10_10_10](./output_n6_A_10_10_10.png)  | ![n7_A_10_10_10](./output_n7_A_10_10_10.png) | ![n8_A_10_10_10](./output_n8_A_10_10_10.png) |
 
-この結果からわかるように，Green関数の実際の値は，${\bf c}$によって変わらないが，$G_{\rm apx}$の値は${\bf c}$によって変化し，
-${\bf c}$が${\bf x}$に近いところでは，$G_{\rm apx}$の値は$G$の値に近づく．
+この結果からわかるように，Green関数の実際の値は，$`{\bf c}`$によって変わらないが，$`G_{\rm apx}`$の値は$`{\bf c}`$によって変化し，
+$`{\bf c}`$が$`{\bf x}`$に近いところでは，$`G_{\rm apx}`$の値は$`G`$の値に近づく．
 
-$a_{near},b_{near}$は，より小さければ精度が良く，
-また，$a_{far},b_{far}$は，より大きければ精度が良くなる．
+$`a_{near},b_{near}`$は，より小さければ精度が良く，
+また，$`a_{far},b_{far}`$は，より大きければ精度が良くなる．
 
 */
 
@@ -144,9 +144,9 @@ double Gapx(unsigned p,
 
 /*DOC_EXTRACT BEM
 
-### $G_{\rm apx}$の勾配$\nabla G_{\rm apx}$の精度
+### $`G_{\rm apx}`$の勾配$`\nabla G_{\rm apx}`$の精度
 
-$\nabla G_{\rm apx}$は，$\nabla_{\rm \circ}=(\frac{\partial}{\partial r},\frac{\partial}{\partial a},\frac{\partial}{\partial b})$とすると，
+$`\nabla G_{\rm apx}`$は，$`\nabla_{\rm \circ}=(\frac{\partial}{\partial r},\frac{\partial}{\partial a},\frac{\partial}{\partial b})`$とすると，
 
 ```math
 \nabla G_{\rm apx} =
