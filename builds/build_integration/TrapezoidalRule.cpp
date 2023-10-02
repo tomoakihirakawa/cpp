554ff20defb4447d1f90dd58e45beb3c0f047495
平川知明 <tomoaki@hirakawatomoakinoMacBook-Pro.local>
2023-10-02 22:38:26 +0900

    add integration

--- builds/build_integration/TrapezoidalRule.cpp
+++ builds/build_integration/TrapezoidalRule.cpp
@@ -1,6 +1,20 @@
 #include <cmath>
 #include <iostream>
 
+/*DOC_EXTRACT 0_0_0_integration_TrapezoidalRule
+
+# 数値積分
+
+## 台形則
+
+台形則は，関数の積分を台形の面積の和で近似する方法である．
+
+```math
+\int_a^b f(x) dx \approx \left(\frac{f(a)+f(b)}{2} + \sum_{i=1}^{N-1} f(a+i\Delta x)\right)\Delta x, \quad \Delta x = \frac{b-a}{N}
+```
+
+*/
+
 template <typename Func, typename arg>
 auto TrapezoidalRule(const Func& f, const arg& a, const arg& b, const int N) {
    auto integral = (f(a) + f(b)) / 2.;
